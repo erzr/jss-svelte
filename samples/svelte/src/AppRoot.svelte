c00ce5448260ad2d4b7fd18813ea2fbe292d28d0
erzr <aelamarre@gmail.com>
2019-08-18 11:17:15 -0400

    removed unused AppRoot.

--- samples/svelte/src/AppRoot.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { onMount } from "svelte";
-  import i18nInit from "./i18n";
-  import App from "./App.svelte";
-
-  export let graphQLClient = null;
-</script>
-
-<App graphQLClient={graphQLClient} />
