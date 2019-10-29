b3d90c6150cf0ab3b6ffc4f076bb5f1dbd9ca208
erzr <aelamarre@gmail.com>
2019-10-29 18:18:16 -0400

    #15, bye axios.

--- samples/svelte/src/dataFetcher.js
+++ samples/svelte/src/dataFetcher.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import 'cross-fetch/polyfill';
 
 /**
- * Implements a data fetcher using Axios - replace with your favorite
+ * Implements a data fetcher using fetch - replace with your favorite
  * SSR-capable HTTP or fetch library if you like. See HttpJsonFetcher<T> type
  * in sitecore-jss library for implementation details/notes.
  * @param {string} url The URL to request; may include query string
  * @param {any} data Optional data to POST with the request.
  */
 export function dataFetcher(url, data) {
-  return axios({
-    url,
+  return fetch(url, {
     method: data ? 'POST' : 'GET',
-    data,
-    // note: axios needs to use `withCredentials: true` in order for Sitecore cookies to be included in CORS requests
-    // which is necessary for analytics and such
-    withCredentials: true,
-  });
+    credentials: 'include',
+    body: JSON.stringify(data)
+  }).then(response => ({
+    status: response.status,
+    statusText: response.statusText,
+    data: response.json()
+  }));
 }
