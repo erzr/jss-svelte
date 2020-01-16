8edbd88769013fce2f2f80954843fc5ecc404f4c
erzr <aelamarre@gmail.com>
2020-01-16 07:41:06 -0500

    cleaned up i18n promise.

--- samples/svelte/src/i18n.js
+++ samples/svelte/src/i18n.js
@@ -2,21 +2,15 @@ import fetch from 'cross-fetch';
 import config from './temp/config';
 
 export default function i18nInit(language, dictionary) {
-  return new Promise((resolve) => {
-    if (dictionary) {
-      resolve({
-        lang: language,
-        phrases: dictionary
-      })
-    } else {
-      // eslint-disable-next-line prettier/prettier
-      const dictionaryServicePath = `${config.sitecoreApiHost}/sitecore/api/jss/dictionary/${config.jssAppName}/${language}?sc_apikey=${config.sitecoreApiKey}`;
+  if (dictionary) {
+    return Promise.resolve({
+      lang: language,
+      phrases: dictionary
+    })
+  } else {
+    const dictionaryServicePath = `${config.sitecoreApiHost}/sitecore/api/jss/dictionary/${config.jssAppName}/${language}?sc_apikey=${config.sitecoreApiKey}`;
 
-      fetch(dictionaryServicePath)
-        .then(response => response.json())
-        .then(json => {
-          resolve(json);
-        });
-    }
-  });
+    return fetch(dictionaryServicePath)
+      .then(response => response.json());
+  }
 }
