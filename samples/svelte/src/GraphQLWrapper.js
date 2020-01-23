f5503d001142ac6973dc29a0c8771aea1a9ddcc4
erzr <aelamarre@gmail.com>
2020-01-22 20:19:23 -0500

    #27, graphql:update integration, a little client rework.

--- samples/svelte/src/GraphQLWrapper.js
+++ samples/svelte/src/GraphQLWrapper.js
@@ -1,6 +1,8 @@
 import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import { BatchHttpLink } from 'apollo-link-batch-http';
+
+import introspectionQueryResultData from './temp/GraphQLFragmentTypes.json';
 
 export default class GraphQLWrapper {
 
@@ -22,7 +24,11 @@ export default class GraphQLWrapper {
     }
 
     this.client = new ApolloClient({
-      cache: new InMemoryCache(),
+      cache: new InMemoryCache({
+        fragmentMatcher: new IntrospectionFragmentMatcher({
+          introspectionQueryResultData,
+        }),
+      }),
       link: new BatchHttpLink(linkOptions)
     });
   }
@@ -61,8 +67,6 @@ export default class GraphQLWrapper {
     if (this.cachePromises) {
       this.promises.push(queryPromise);
     }
-
-    callback(null);
   }
 
   needsToWait() {
