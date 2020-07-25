9aa6dd51fab405af9524818981f4405ac11b761e
erzr <aelamarre@gmail.com>
2020-07-25 13:49:59 -0400

    datefield unit tests.

--- packages/jss-svelte/src/components/DateField.spec.js
+++ packages/jss-svelte/src/components/DateField.spec.js
@@ -1,10 +1,46 @@
 import DateField from './DateField.svelte';
 import { render, fireEvent } from '@testing-library/svelte'
 
-it('it works', async () => {
-    const { getByText, getByTestId } = render(DateField, { field: { value: 'Test' } })
+describe('<DateField />', () => {
+    it('should return null if no editable or value', async () => {
+        const { container } = render(DateField, { field: null })
+        expect(container.textContent).toHaveLength(0);
+    });
 
-    const text = getByText('Test');
+    it('should render value', () => {
+        const { container } = render(DateField, { field: { value: '23-11-2001' } })
+        expect(container.textContent).toEqual('23-11-2001');
+    });
 
-    expect(text).not.toBeNull();
+    it('should render value using render prop function', () => {
+        const { container } = render(DateField, {
+            field: {
+                value: '11-23-2001'
+            },
+            render: (date) => date ? date.toDateString() : ''
+        });
+        expect(container.textContent).toEqual('Fri Nov 23 2001');
+    });
+
+    it('should render null value using render prop function', () => {
+        const { container } = render(DateField, {
+            field: {
+                value: undefined,
+                editable: 'xxx'
+            },
+            editable: false,
+            render: (date) => date ? date.toDateString() : ''
+        });
+        expect(container.textContent).toHaveLength(0);
+    });
+
+    it('should render editable value', () => {
+        const { container } = render(DateField, {
+            field: {
+                editable: '<h1 class="super">11-23-2001</h1>'
+            },
+            editable: true
+        });
+        expect(container.innerHTML).toEqual('<div><h1 class="super">11-23-2001</h1></div>');
+    });
 });
--- packages/jss-svelte/src/components/DateField.svelte
+++ packages/jss-svelte/src/components/DateField.svelte
@@ -1,5 +1,5 @@
 <script type="ts">
-  import type { Field } from '@sitecore-jss/sitecore-jss';
+  import type { Field } from "@sitecore-jss/sitecore-jss";
 
   export let field: Field<string> = null;
   export let editable = true;
@@ -8,13 +8,16 @@
   export let renderComponent = null;
 
   const hasField = field && (field.editable || field.value);
-  const setDangerously = (field.editable && editable) || !encode;
 
-  let output = field.editable && editable ? field.editable : field.value;
+  let setDangerously;
+  let output;
 
   if (hasField) {
+    setDangerously = (field.editable && editable) || !encode;
+    output = field.editable && editable ? field.editable : field.value;
+
     if (render) {
-      output = render(new Date(output));
+      output = render(output ? new Date(output) : null);
     }
   }
 </script>
